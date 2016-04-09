fcd9d1ec728a69cf2dbfacc1b27a68a1b9302e90
Patience <chenps10@gmail.com>
2016-04-09 18:04:18 +0800

    重新修改

--- set/assets/js/main.js
+++ set/assets/js/main.js
@@ -1,3 +1,7 @@
+---
+layout: null
+---
+
 $(function() {
   var toc     = $('.toc-link'),
       sidebar = $('#sidebar'),
@@ -13,7 +17,13 @@ $(function() {
     }).attr('target', '_blank');
 
     // discus comment.
-    
+    {% if site.disqus_shortname %}
+    (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//{{ site.disqus_shortname }}' + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+    {% endif %}
 
     // your scripts
   };
@@ -40,7 +50,10 @@ $(function() {
       NProgress.done();
       main.scrollTop(0).addClass('fadeIn');
       menu.add(sidebar).removeClass('open');
-      
+      {% if site.google_analytics %}
+      ga('set', 'location', window.location.href);
+      ga('send', 'pageview');
+      {% endif %}
     }
   });
 
